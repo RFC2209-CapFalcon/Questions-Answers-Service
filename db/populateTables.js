5aa5be0c7ab381ffb82583b4f7d9d2959cd3292a
milrilowe <milesrhowens@gmail.com>
2022-11-08 21:55:03 -0700

    Fixed logic so entire file will execute despite any bugs

--- db/populateTables.js
+++ db/populateTables.js
@@ -1,10 +1,9 @@
+require('dotenv').config();
+
 const db = require('./index.js');
 const fs = require('fs');
 
-/**
- * Select database (I think this is what this does)
- */
-db.query('SELECT NOW()');
+const PATH_TO_DATA = process.env.PATH_TO_DATA;
 
 /**
  * Create questions table and populate if necessary
@@ -18,52 +17,62 @@ db.query(`CREATE TABLE IF NOT EXISTS questions(
   asker_email VARCHAR(60) NOT NULL,
   reported BOOLEAN NOT NULL,
   helpful INTEGER NOT NULL DEFAULT 0)`)
+.then(() => {
+  db.query(`CREATE TABLE IF NOT EXISTS answers (
+    id SERIAL PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    body VARCHAR(1000) NOT NULL,
+    date_written VARCHAR(15) NOT NULL,
+    answerer_name VARCHAR(60) NOT NULL,
+    answerer_email VARCHAR(60) NOT NULL,
+    reported BOOLEAN NOT NULL,
+    helpful INTEGER NOT NULL DEFAULT 0,
+    FOREIGN KEY (question_id) REFERENCES questions(id))`)
   .then(() => {
-    fs.existsSync('/home/miles/Desktop/Hack-Reactor/questions-answers-service/csv-data/questions.csv', (exists) => {
-      if (exists) {
-        db.query(`COPY questions FROM '/home/miles/Desktop/Hack-Reactor/questions-answers-service/csv-data/questions.csv' csv header;`);
-      }
-    })
-  })
-  .then(() => {
-    /**
-     * Create answers table and populate if necessary
-     */
-    const createAnswers = db.query(`CREATE TABLE IF NOT EXISTS answers (
-      id SERIAL PRIMARY KEY,
-      question_id INTEGER NOT NULL,
-      body VARCHAR(1000) NOT NULL,
-      date_written VARCHAR(15) NOT NULL,
-      answerer_name VARCHAR(60) NOT NULL,
-      answerer_email VARCHAR(60) NOT NULL,
-      reported BOOLEAN NOT NULL,
-      helpful INTEGER NOT NULL DEFAULT 0,
-      FOREIGN KEY (question_id) REFERENCES questions(id))`)
-      .then(() => {
-        fs.existsSync('/home/miles/Desktop/Hack-Reactor/questions-answers-service/csv-data/answers.csv', (exists) => {
-          if (exists) {
-            db.query(`COPY answers FROM '/home/miles/Desktop/Hack-Reactor/questions-answers-service/csv-data/answers.csv' csv header;`);
-          }
-        });
-      })
-      .then(() => {
-        /**
-         * Create photos table and populate if necessary
-         */
-        const photos = db.query(`CREATE TABLE IF NOT EXISTS photos (
-          id SERIAL NOT NULL PRIMARY KEY,
-          answer_id INTEGER NOT NULL,
-          url VARCHAR(2048) NOT NULL,
-          FOREIGN KEY (answer_id) REFERENCES answers(id))`)
-          .then(() => {
-            fs.existsSync('/home/miles/Desktop/Hack-Reactor/questions-answers-service/csv-data/answers_photos.csv', (exists) => {
-              if (exists) {
-                db.query(`COPY photos FROM '/home/miles/Desktop/Hack-Reactor/questions-answers-service/csv-data/answers_photos.csv' csv header;`);
+    db.query(`CREATE TABLE IF NOT EXISTS photos (
+      id SERIAL NOT NULL PRIMARY KEY,
+      answer_id INTEGER NOT NULL,
+      url VARCHAR(2048) NOT NULL,
+      FOREIGN KEY (answer_id) REFERENCES answers(id))`)
+    .then(() => {
+      /**
+       * Populates questions/answers/photos tables with data from csv.
+       */
+      const canPopulateQuestionsTable = fs.existsSync(`${PATH_TO_DATA}/questions.csv`);
+      const canPopulateAnswersTable =  fs.existsSync(`${PATH_TO_DATA}/answers.csv`);
+      const canPopulatePhotosTable = fs.existsSync(`${PATH_TO_DATA}/answers_photos.csv`);
+
+      if (canPopulateQuestionsTable) {
+        db.query(`COPY questions FROM '${PATH_TO_DATA}/questions.csv' csv header;`)
+        .catch(() => {
+          console.log('Did not copy data from questions csv - this usually means the data has already been copied.');
+        })
+        .then(() => {
+          if (canPopulateAnswersTable) {
+            db.query(`COPY answers FROM '${PATH_TO_DATA}/answers.csv' csv header;`)
+            .catch(() => {
+              console.log('Did not copy data from answers csv - this usually means the data has already been copied.');
+            })
+            .then(() => {
+              if (canPopulatePhotosTable) {
+                db.query(`COPY photos FROM '${PATH_TO_DATA}/answers_photos.csv' csv header;`)
+                .catch(() => {
+                  console.log('Did not copy data from answers_photos csv - this usually means the data has already been copied.');
+                })
+                .then(() => {
+                  console.log('Finished seeding.');
+                });
+              } else {
+                console.log('Could not find photos.csv - did not copy');
               }
             });
-          });
-      });
+          } else {
+            console.log('Could not find answers.csv - did not copy');
+          }
+        });
+      } else {
+        console.log('Could not find questions.csv - did not copy');
+      }
+    });
   });
-
-
-
+});
