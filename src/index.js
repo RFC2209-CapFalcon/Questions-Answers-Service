20fd94371b346e5981b9c45238fb66e61170b31b
milrilowe <milesrhowens@gmail.com>
2022-11-12 06:25:23 -0700

    Added 2 more express instances

--- src/index.js
+++ src/index.js
@@ -14,8 +14,24 @@ app.use(express.json());
 app.use(authorize);
 app.use('/qa', qaRoutes);
 
+const app2 = express();
+app2.use(express.json());
+app2.use(authorize);
+app2.use('/qa', qaRoutes);
+
+const app3 = express();
+app3.use(express.json());
+app3.use(authorize);
+app3.use('/qa', qaRoutes);
+
 
 app.listen(PORT);
-console.log(`Listening at ${BASE_URL}:${PORT}`);
+console.log(`Server 1 listening at ${BASE_URL}:${PORT}`);
+
+app2.listen(Number(PORT) + 1);
+console.log(`Server 2 listening at ${BASE_URL}:${Number(PORT) + 1}`);
+
+app3.listen(Number(PORT) + 2);
+console.log(`Server 3 listening at ${BASE_URL}:${Number(PORT) + 2}`);
 
 module.exports = app;
